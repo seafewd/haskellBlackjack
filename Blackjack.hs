8cea395b6cb26043286d81a5c953e1c1e59fb2a7
alex <alexander.korpas@protonmail.com>
2020-09-10 14:20:51 +0200

    gameover, value and winner functions fixed

--- Blackjack.hs
+++ Blackjack.hs
@@ -2,53 +2,79 @@ module Blackjack where
 
 import Cards
 import RunGame
-import Test.QuickCheck hiding (shuffle)
 
--- Define cards
-aceOfHearts :: Card
-aceOfHearts = Card Ace Hearts
-aceOfSpades :: Card
-aceOfSpades = Card Ace Spades
-aceOfDiamonds :: Card
-aceOfDiamonds = Card Ace Diamonds
-threeOfClubs :: Card
-threeOfClubs = Card (Numeric 3) Clubs
+------- Some cards --------
+aCard1 :: Card
+aCard1 = Card (Numeric 8) Spades
+
+aCard2 :: Card
+aCard2 = Card Jack Hearts
+
+ace_of_spades :: Card
+ace_of_spades = Card Ace Spades
+
+c1 :: Card
+c1 = Card (Numeric 6) Spades
+
+c2 :: Card
+c2 = Card Jack Hearts
+
+c3 :: Card
+c3 = Card Ace Hearts
+
+c4 :: Card
+c4 = Card Ace Diamonds
+
+c5 :: Card
+c5 = Card Ace Spades
+
+c6 :: Card
+c6 = Card Ace Clubs
 
 
--- Put cards in a hand
-hand :: Hand
-hand = (aceOfHearts:aceOfSpades:threeOfClubs:aceOfDiamonds:[])
+------- Some hands --------
+aHand1 :: [Card]
+aHand1 = [aCard1, ace_of_spades]
+
+pimpHand :: Hand
+pimpHand = [c1,c2,c3,c4,c5,c6]
+
+bankHand :: Hand
+bankHand = [c2, c3]
+
+badHand :: Hand
+badHand = [c1, c2, c2]
 
 
--- display each card in this hand as list of striding
+-- create a list of strings containing each card
 display :: Hand -> [String]
-display [] = []
-display (c:cs) = displayCard c : display cs
+display h = [displayCard c | c <- h]
 
 
 -- display the card as string
 displayCard :: Card -> String
-displayCard (Card rank suit) = show rank ++ " of " ++ show suit
+displayCard (Card r s) = show r ++ " of " ++ show s
 
 
 -- calculate the value of a card
 valueCard :: Card -> Int
-valueCard (Card r s) = valueRank (Card r s) -- + valueSuit (Card _ s)
+valueCard (Card r s) = valueRank r
 
 
 -- calculate the value of a rank
-valueRank :: Card -> Int
-valueRank (Card (Numeric i) _) = i
-valueRank (Card Ace _) = 11        -- TODO check value for ACE
-valueRank (Card _ _ ) = 10
+valueRank :: Rank -> Int
+valueRank (Numeric n) = n
+valueRank Ace = 11
+valueRank _ = 10
 
 
 -- calculate the value of the cards in this hand
--- go through the list of cards
--- for each card, add value of card to sum, then return sum
 value :: Hand -> Int
 value [] = 0
-value (c:cs) = valueCard c + value cs
+value h
+    | val > 21 = val - (numberOfAces h) * 10
+    | otherwise = val
+    where val = sum [valueCard c | c <- h]
 
 
 -- get number of aces in a hand
@@ -56,13 +82,16 @@ numberOfAces :: Hand -> Int
 -- filter the list of Cards where x has Rank Ace and get the length of that list
 numberOfAces cs = length (filter (\x -> rank x == Ace) cs)
 
+
 -- check if bust
 gameOver :: Hand -> Bool
-gameOver cs = value cs > 21
+gameOver h = not (value h < 22)
 
 
 -- check if winner
 winner :: Hand -> Hand -> Player
-winner cb cg
-    | value cg > value cb = Guest
-    | otherwise = Bank
+winner g b 
+    | gameOver g = Bank
+    | gameOver b = Guest
+    | value g <= value g = Bank
+    | otherwise = Guest
